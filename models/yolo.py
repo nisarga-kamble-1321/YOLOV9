1f295723f0d6fc9674a78cd76f2cb0e6c741eefb
Nisarga Kamble <105123774+nisarga-kamble-1321@users.noreply.github.com>
2026-04-13 19:37:37 +0530

    Update yolo.py Detect Class cv layers replaced cv2, cv3

--- models/yolo.py
+++ models/yolo.py
@@ -46,10 +46,23 @@ class Detect(nn.Module):
         self.stride = torch.zeros(self.nl)  # strides computed during build
 
         c2, c3 = max((ch[0] // 4, self.reg_max * 4, 16)), max((ch[0], min((self.nc * 2, 128))))  # channels
-        self.cv2 = nn.ModuleList(
-            nn.Sequential(Conv(x, c2, 3), Conv(c2, c2, 3), nn.Conv2d(c2, 4 * self.reg_max, 1)) for x in ch)
+        self.cv2 = nn.ModuleList( nn.Sequential( Conv(x, c2, 3),
+            nn.Conv2d(c2, c2, 3, padding=1, groups=c2),  # 🔥 depthwise
+            nn.BatchNorm2d(c2),
+            nn.SiLU(),
+            nn.Conv2d(c2, 4 * self.reg_max, 1)
+            ) for x in ch
+                        )
+
         self.cv3 = nn.ModuleList(
-            nn.Sequential(Conv(x, c3, 3), Conv(c3, c3, 3), nn.Conv2d(c3, self.nc, 1)) for x in ch)
+            nn.Sequential(
+            Conv(x, c3, 3),
+            nn.Conv2d(c3, c3, 3, padding=1, groups=c3),  # 🔥 depthwise
+            nn.BatchNorm2d(c3),
+            nn.SiLU(),
+            nn.Conv2d(c3, self.nc, 1)
+                ) for x in ch
+            )
         self.dfl = DFL(self.reg_max) if self.reg_max > 1 else nn.Identity()
 
     def forward(self, x):
